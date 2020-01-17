9f5658ffa34ead7512c29d320a6ac9904c46662d
Bani Tariveh <bani.tariveh@gmail.com>
2020-01-18 00:21:50 +0200

    Cleanup.

--- main.ino
+++ main.ino
@@ -1,16 +1,20 @@
 /*
-The program reads sensor data from IR obstacle sensor and turns on the
-lights if it detects no obstacles in front of it.
+The program reads sensor data from an IR obstacle
+sensor and turns on the lights if it detects no
+obstacles in front of it.
 */
 
-int timer = 1000;  // Delay between sensor reads.
+
+int interval = 1000;  // Delay between sensor reads.
 int signalPin = A0;  // Analog pin for signal from sensor.
 int ledPin = 3;  //  This pin turns on the MOSFET that will turn on the LEDS.
 
 void setup() {
-
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
+  sensorData = analogRead(signalPin);
+  delay(timer)
 
 }
