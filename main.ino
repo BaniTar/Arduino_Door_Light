62fdff5f7776cd7944e7f944e499239a3e4e7382
Bani Tariveh <bani.tariveh@gmail.com>
2020-01-17 23:47:49 +0200

    Initial stub.

---
+++ main.ino
@@ -0,0 +1,11 @@
+int timer = 1000;  // Delay between sensor reads.
+int signalPin = A0;  // Analog pin for signal from sensor.
+int ledPin = 3;  //  This pin turns on the MOSFET that will turn on the LEDS.
+
+void setup() {
+
+}
+
+void loop() {
+
+}
