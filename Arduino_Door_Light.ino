4baa1128f990927ff18beabcec802c7a3dfb19a0
Bani Tariveh <bani.tariveh@gmail.com>
2020-02-12 20:06:02 +0200

    adding wakeup functionality

--- Arduino_Door_Light.ino
+++ Arduino_Door_Light.ino
@@ -5,7 +5,7 @@ obstacles in front of it. It works with
 */
 
 #include <avr/sleep.h> // Library for Arduino sleep functions
-#define interruptPin 2
+#define interruptPin 3
 
 int signalPin = 3;  // Digital pin for signal from sensor.
 int ledPin = 2;  //  This pin turns on the MOSFET that will turn on the LEDS.
@@ -14,6 +14,8 @@ void setup() {
     pinMode(ledPin, OUTPUT);
     digitalWrite(ledPin, LOW);
     pinMode(signalPin, INPUT_PULLUP);
+    // The controller will detect if voltage has gone from LOW to HIGH.
+    attachInterrupt(digitalPinToInterrupt(interruptPin), wakeUp, RISING );
 }
 
 void loop() {
@@ -21,5 +23,11 @@ void loop() {
 }
 
 void wakeUp() {
-    //stub
+    // Wake up CPU and activate LED strip if the door is still open.
+    sleep_disable();
+    while ( signalPin == HIGH) {
+        digitalWrite(ledPin, HIGH);
+        // Wait for a second until checking again to if door is closed.
+        delay(1000);
+    }
 }
